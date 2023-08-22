bef002eee3ae5382f391dbf986e77a7021d499dc
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-08-22 15:44:37 -0300

    Create login system

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $validate = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credential = $request->only('email', 'password');
+
+        if(auth()->attempt($credential)) {
+            $user = auth()->user();
+            $token = $user->createToken('token-name')->plainTextToken;
+
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out'], 200);
+    }
+}
