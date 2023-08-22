9f769113f407db336245fc4701c63c6cd2c38096
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2023-08-22 15:11:36 -0300

    Create models for tasks and files

---
+++ app/Models/Task.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $table = 'task';
+    
+    protected $fillable = [
+        'id',
+        'title',
+        'description',
+        'completed',
+        'completed_date',
+        'deleted_date',
+        'created_at',
+        'updated_at',
+        'created_by',
+        'updated_by'
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function files()
+    {
+        return $this->hasMany(Files::class, 'task_id');
+    }
+}
